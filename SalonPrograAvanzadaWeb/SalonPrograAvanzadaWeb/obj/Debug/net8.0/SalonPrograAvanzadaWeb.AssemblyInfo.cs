d87e7a8fde3e4e5f0321bc9962c8cecc64695061
Axel Noel Cabezas Cerda <114128602+AxelCab17@users.noreply.github.com>
2024-03-08 15:50:21 -0600

    Add API

--- SalonPrograAvanzadaWeb/SalonPrograAvanzadaWeb/obj/Debug/net8.0/SalonPrograAvanzadaWeb.AssemblyInfo.cs
+++ SalonPrograAvanzadaWeb/SalonPrograAvanzadaWeb/obj/Debug/net8.0/SalonPrograAvanzadaWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalonPrograAvanzadaWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+074fe074acdfa79f10d52f5b965d2354591ea97f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16261894456caf2625631f1025592eb013286b53")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalonPrograAvanzadaWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalonPrograAvanzadaWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
