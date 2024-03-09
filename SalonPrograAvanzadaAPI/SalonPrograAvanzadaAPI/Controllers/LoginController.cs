28b38e6e82a23711522c848ee85e6ae40b7b5a14
Czumbado <carloszumbadocardenas@gmail.com>
2024-03-08 22:39:16 -0600

    API
    Registro,Login.Recuperar,CambioContraseña

---
+++ SalonPrograAvanzadaAPI/SalonPrograAvanzadaAPI/Controllers/LoginController.cs
@@ -0,0 +1,198 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Dapper;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using SalonPrograAvanzadaAPI.Entities;
+using Microsoft.AspNetCore.Authorization;
+using System.Data.Common;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace SalonPrograAvanzadaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IUtilitarios _utilitarios;
+        private string _connection;
+
+        public LoginController(IConfiguration configuration, IUtilitarios utilitarios)
+        {
+            _configuration = configuration;
+            _connection = _configuration.GetConnectionString("DefaultConnection");
+            _utilitarios = utilitarios;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("IniciarSesion")]
+        public IActionResult IniciarSesion(UserEnt entidad)
+        {
+            try
+            {
+
+                entidad.PasswordUser = GetSHA256Hash(entidad.PasswordUser);
+
+                using (var context = new SqlConnection(_connection))
+                {
+                    var datos = context.ExecuteReader("Login",
+                       new { entidad.Email, entidad.PasswordUser },
+                        commandType: CommandType.StoredProcedure);
+
+                    if (datos.Read())
+                    {
+                        var resultado = new
+                        {
+                            IdUser = datos["IdUser"],
+                            IdRol = datos["IdRol"],
+                            Identification = datos["Identification"],
+                            NameUser = datos["NameUser"],
+                            LastName = datos["LastName"],
+                            Email = datos["Email"],
+                            Phone = datos["Phone"]
+                        };
+
+                        return Ok(resultado);
+                    }
+                    else
+                    {
+                        return NotFound();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("ConsultNationality")]
+        public IActionResult ConsultNationality()
+        {
+            try
+            {
+
+                using (var context = new SqlConnection(_connection))
+                {
+                    var datos = context.Query<SelectListItem>("ConsultNationality",
+                        new { },
+                        commandType: CommandType.StoredProcedure).ToList();
+                    return Ok(datos);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("RegistrerAccount")]
+        public IActionResult RegisterAccount(UserEnt entidad)
+        {
+            try
+            {
+                entidad.PasswordUser = GetSHA256Hash(entidad.PasswordUser);
+                entidad.ConfPassUser = GetSHA256Hash(entidad.ConfPassUser);
+
+                using (var context = new SqlConnection(_connection))
+                {
+                    var datos = context.Execute("RegistrerAccount",
+                       new { entidad.IdNationality, entidad.Identification, entidad.NameUser, entidad.LastName, entidad.Email, entidad.PasswordUser, entidad.ConfPassUser, entidad.Phone },
+                        commandType: CommandType.StoredProcedure);
+
+                    return Ok(datos);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("RecoverAccount")]
+        public IActionResult RecoverAccount(UserEnt entidad)
+        {
+            try
+            {
+                using (var context = new SqlConnection(_connection))
+                {
+                    var datos = context.Query<UserEnt>("RecoverAccount",
+                        new { entidad.Email },
+                        commandType: CommandType.StoredProcedure).FirstOrDefault();
+
+                    if (datos != null)
+                    {
+                        string TempKey = _utilitarios.GenerarCodigo();
+                        string contenido = _utilitarios.ArmarHTML(datos, TempKey);
+
+                        context.Execute("UpdateTempKey",
+                            new { datos.IdUser, TempKey },
+                            commandType: CommandType.StoredProcedure);
+
+                        _utilitarios.EnviarCorreo(datos.Email, "¡Restaurar Contraseña!", contenido);
+                        return Ok(1);
+                    }
+                    else
+                        return Ok(0);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("ChangePassword")]
+        public IActionResult ChangePassword(UsuarioRecoverEnt entidad)
+        {
+            try
+            {
+                entidad.PasswordUser = GetSHA256Hash(entidad.PasswordUser);
+
+                using (var context = new SqlConnection(_connection))
+                {
+                    var datos = context.Execute("ChangePassword",
+                       new { entidad.Codigo, entidad.Email, entidad.PasswordUser },
+                        commandType: CommandType.StoredProcedure);
+
+                    return Ok(datos);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        private string GetSHA256Hash(string input)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // Convertir la cadena de entrada a un array de bytes y computar el hash
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+                // Convertir el array de bytes a una cadena hexadecimal
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+    }
+}
+
---
+++ SalonPrograAvanzadaAPI/SalonPrograAvanzadaAPI/Entities/Utilitarios.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.Extensions.Configuration;
+//using Microsoft.IdentityModel.Tokens;
+//using System.IdentityModel.Tokens.Jwt;
+using System.Net.Mail;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SalonPrograAvanzadaAPI.Entities
+{
+    public class Utilitarios : IUtilitarios
+    {
+        private readonly IConfiguration _configuration;
+        private IHostEnvironment _hostingEnvironment;
+
+        public Utilitarios(IConfiguration configuration, IHostEnvironment hostingEnvironment)
+        {
+            _configuration = configuration;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public string ArmarHTML(UserEnt datos, string claveTemporal)
+        {
+            string rutaArchivo = Path.Combine(_hostingEnvironment.ContentRootPath, "CorreosTemplate\\Contrasenna.html");
+            string htmlArchivo = System.IO.File.ReadAllText(rutaArchivo);
+            htmlArchivo = htmlArchivo.Replace("@@Nombre", datos.NameUser);
+            htmlArchivo = htmlArchivo.Replace("@@ClaveTemporal", claveTemporal);
+            //htmlArchivo = htmlArchivo.Replace("@@Link", "https://localhost:7241/Login/CambiarClaveCuenta?q=" + datos.IdUser.ToString());
+
+            return htmlArchivo;
+        }
+
+        public string GenerarCodigo()
+        {
+            int length = 4;
+            const string valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            StringBuilder res = new StringBuilder();
+            Random rnd = new Random();
+            while (0 < length--)
+            {
+                res.Append(valid[rnd.Next(valid.Length)]);
+            }
+            return res.ToString();
+        }
+
+        public void EnviarCorreo(string Destinatario, string Asunto, string Mensaje)
+        {
+            string correoSMTP = _configuration.GetSection("Llaves:correoSMTP").Value;
+            string claveSMTP = _configuration.GetSection("Llaves:claveSMTP").Value;
+
+            MailMessage msg = new MailMessage();
+            msg.To.Add(new MailAddress(Destinatario));
+            msg.From = new MailAddress(correoSMTP);
+            msg.Subject = Asunto;
+            msg.Body = Mensaje;
+            msg.IsBodyHtml = true;
+
+            SmtpClient client = new SmtpClient();
+            client.UseDefaultCredentials = false;
+            client.Credentials = new System.Net.NetworkCredential(correoSMTP, claveSMTP);
+            client.Port = 587;
+            client.Host = "smtp.office365.com";
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.EnableSsl = true;
+            client.Send(msg);
+        }
+
+        //public string GenerarToken(string idUsuario)
+        //{
+        //    List<Claim> claims = new List<Claim>
+        //    {
+        //        new Claim(ClaimTypes.Name, Encrypt(idUsuario))
+        //    };
+
+        //    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Ty1UELmVFKQmMD4af0a4jvfZS30cXu3U"));
+        //    var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+        //    var token = new JwtSecurityToken(
+        //        claims: claims,
+        //        expires: DateTime.UtcNow.AddMinutes(10),
+        //        signingCredentials: cred);
+
+        //    return new JwtSecurityTokenHandler().WriteToken(token);
+        //}
+
+        public string Encrypt(string texto)
+        {
+            byte[] iv = new byte[16];
+            byte[] array;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes("EzfjS0IHnNSjv0jo");
+                aes.IV = iv;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))
+                        {
+                            streamWriter.Write(texto);
+                        }
+
+                        array = memoryStream.ToArray();
+                    }
+                }
+            }
+
+            return Convert.ToBase64String(array);
+        }
+
+        public string Decrypt(string texto)
+        {
+            byte[] iv = new byte[16];
+            byte[] buffer = Convert.FromBase64String(texto);
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes("EzfjS0IHnNSjv0jo");
+                aes.IV = iv;
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream(buffer))
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))
+                        {
+                            return streamReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
+
