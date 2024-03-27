4622351ff4b311db89aa67b9ac62924f4808014f
JuanCarlosSantamaria <juank.0529.sa@gmail.com>
2024-03-27 17:54:07 -0600

    Update SP y TRG

--- SalonPrograAvanzadaWeb/SalonPrograAvanzadaWeb/obj/Debug/net8.0/SalonPrograAvanzadaWeb.AssemblyInfo.cs
+++ SalonPrograAvanzadaWeb/SalonPrograAvanzadaWeb/obj/Debug/net8.0/SalonPrograAvanzadaWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalonPrograAvanzadaWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1fea2a8e8bd2eb2f493402340e825a0be947032")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45acdc12c55f2a57c16eb6fa1ca45de01bb9c061")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalonPrograAvanzadaWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalonPrograAvanzadaWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
