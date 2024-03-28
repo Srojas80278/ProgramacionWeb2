b2140928e8b345cdb096beadc94406e3b8a3cec1
Srojas80278 <Srojas80278@gmail.com>
2024-03-27 22:05:54 -0600

    ADD SCRIPT

--- SalonPrograAvanzadaWeb/SalonPrograAvanzadaWeb/obj/Debug/net8.0/SalonPrograAvanzadaWeb.AssemblyInfo.cs
+++ SalonPrograAvanzadaWeb/SalonPrograAvanzadaWeb/obj/Debug/net8.0/SalonPrograAvanzadaWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalonPrograAvanzadaWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ec06b0d37aae3519083d33a9f335d375449d3af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a3c1de825c1baff480a1fb0c5f576eb7c88608c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalonPrograAvanzadaWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalonPrograAvanzadaWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
